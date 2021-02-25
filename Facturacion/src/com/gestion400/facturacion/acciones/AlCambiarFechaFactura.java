130d2ea3113d419e588bd64b55736fde2879805d
asilva <asilva@gestion400.com>
2021-02-25 14:25:04 +0100

    Proyecto facturacion

---
+++ Facturacion/src/com/gestion400/facturacion/acciones/AlCambiarFechaFactura.java
@@ -0,0 +1,21 @@
+package com.gestion400.facturacion.acciones;
+
+import java.util.*;
+
+import org.openxava.actions.*;
+
+
+/**
+ * Acción a ejecutar cuando cambie la referencia cliente en el módulo Factura
+ */
+public class AlCambiarFechaFactura extends OnChangePropertyBaseAction{
+
+	@Override
+	public void execute() throws Exception {
+		
+		Date fecha = (Date) getView().getValue("fecha");
+		
+		addWarning("AlCambiarFecha.warning", fecha);
+	}
+
+}
---
+++ Facturacion/src/com/gestion400/facturacion/modelo/EstadoFactura.java
@@ -0,0 +1,12 @@
+package com.gestion400.facturacion.modelo;
+
+import org.openxava.util.*;
+
+public enum EstadoFactura {
+	PENDIENTE, PAGADA;
+	
+	@Override
+	public String toString() {
+		return Labels.get(this.name());
+	}
+}
